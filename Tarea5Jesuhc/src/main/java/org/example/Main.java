9b92c121d2305432bfffbddff2c0d5f217b22fca
jeshc <hcjesus@gmail.com>
2023-02-21 12:38:54 -0600

    Tarea 5 terminada y revisada

---
+++ Tarea5Jesuhc/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String entrada = teclado.nextLine();
+        System.out.println("Capturaste: " + entrada);
+        
+    }
+}
